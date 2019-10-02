675d57ae2bb3423476d559c5b4dde498cd1a7503
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 15:20:20 +0300

    working with the list

--- WorkingWithTheLists/ContentView.swift
+++ WorkingWithTheLists/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        List {
+            RestaurantRow(name: "Joe's Original")
+            RestaurantRow(name: "The Real Joe's Original")
+            RestaurantRow(name: "Original Joe's")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct RestaurantRow: View {
+    var name: String
+
+    var body: some View {
+        Text("Restaurant: \(name)")
     }
 }
