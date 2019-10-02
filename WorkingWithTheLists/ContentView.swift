479b9a25cbaf608beb4f5c168f2f9b2c69bc2f52
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 15:28:33 +0300

    How to let users move rows in a list

--- WorkingWithTheLists/ContentView.swift
+++ WorkingWithTheLists/ContentView.swift
@@ -17,13 +17,14 @@ struct ContentView: View {
                 ForEach(users, id: \.self) { user in
                     Text(user)
                 }
-                .onDelete(perform: delete)
+                .onMove(perform: move)
             }
+            .navigationBarItems(trailing: EditButton())
         }
     }
 
-    func delete(at offsets: IndexSet) {
-        users.remove(atOffsets: offsets)
+    func move(from source: IndexSet, to destination: Int) {
+        users.move(fromOffsets: source, toOffset: destination)
     }
 }
 
